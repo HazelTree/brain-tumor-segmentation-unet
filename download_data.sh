ebbeafcd228dd8418205dd497051995fcd04507c
HazelPi <zhouchenhao_819@hazelzdeMacBook-Pro.local>
2020-05-08 13:57:40 -0700

    fixed bugs accordingly

--- download_data.sh
+++ download_data.sh
@@ -11,13 +11,12 @@ unzip -q temp.zip && rm temp.zip
 
 # concatenate the multiple zipped data in a single zip
 cat brainTumorDataPublic_* > brainTumorDataPublic_temp.zip
-zip -FF brainTumorDataPublic_temp.zip --out data.zip > progress.txt
 
 # remove the temporary files
 rm brainTumorDataPublic_*
 
 # unzip the full archive and delete it 
-unzip -q data.zip -d data && rm data.zip
+unzip -q brainTumorDataPublic_temp.zip -d data && rm brainTumorDataPublic_temp.zip
 
 # check that "data" contains 3064 files
 ls data | wc -l
